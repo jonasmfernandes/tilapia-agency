a6d3b9958ae77765d566ad941b6d50f1d1f48d6d
Jonas Monteiro <mfernandes.jonas@gmail.com>
2025-03-21 02:57:35 -0300

    feat: created services section

--- js/script.js
+++ js/script.js
@@ -2,4 +2,34 @@ $(document).ready(function() {
     $(".menu-custom").on("click", function() {
         $("#rotative-menu").toggleClass("active");
     });
-});
+
+    var currentIndex = 0;
+    var totalItems = $('.serviceContent').length;
+
+    function updateCarousel() {
+        var itemWidth = $('.serviceContent').outerWidth(); 
+        var offset = -(currentIndex * itemWidth); 
+        if ($(window).width() <= 576) {
+            offset = -(currentIndex * 1155); 
+            $('.serviceWrapper').css('transform', 'translateX(' + offset + 'px)');
+        } else {
+            offset = -(currentIndex * 20); 
+            $('.serviceWrapper').css('transform', 'translateX(' + offset + '%)');
+        }
+
+        $('.dot').removeClass('active');
+        $('.dot').eq(currentIndex).addClass('active');
+    }
+
+    $('.dot').on('click', function() {
+        currentIndex = $(this).data('slide');
+        updateCarousel();
+    });
+
+    setInterval(function() {
+        currentIndex = (currentIndex + 1) % totalItems;
+        updateCarousel();
+    }, 5000); 
+
+})
+;
